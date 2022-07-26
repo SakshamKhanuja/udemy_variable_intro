e2085ffb9c657843de0aa3d8beed5c8710360bb0
SakshamKhanuja <ssk@outlook.ie>
2022-07-26 12:48:46 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Declaring and initializing a variable of data type "int" that hold a whole number.
+        int myFirstNumber = 5;
+
+        // Declaring an "int" variable.
+        int mySecondNumber;
+
+        // Initializing "mySecondNumber" via assignment operator "=".
+        mySecondNumber = 10;
+
+        // Prints String literal "myFirstNumber".
+        System.out.println("myFirstNumber");
+
+        // Prints the value stored in "myFirstNumber".
+        System.out.println(myFirstNumber);
+
+        // Prints the value stored in "mySecondNumber".
+        System.out.println(mySecondNumber);
+
+        // Assigning a complex expression to "myFirstNumber".
+        myFirstNumber = 10 + 5;
+
+        // Prints the value stored in "myFirstNumber".
+        System.out.println(myFirstNumber);
+
+        // Assigning a complex expression to "myFirstNumber".
+        myFirstNumber = (10 + 5) + (2 * 10);
+
+        // Prints the value stored in "myFirstNumber".
+        System.out.println(myFirstNumber);
+    }
+}
